40d89bde53016494be8a9790e398193038333e5f
sydomori <sydneyomori@gmail.com>
2026-06-23 00:03:22 +0300

    feat: create handleTabChange function

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -12,3 +12,17 @@ import Typography from '@mui/material/Typography'
 import SearchIcon from '@mui/icons-material/Search'
 import FilterListIcon from '@mui/icons-material/FilterList'
 import LocalShippingIcon from '@mui/icons-material/LocalShipping'
+
+function AppBar(){
+    const navigate = useNavigate()
+    const location = useLocation()
+    const currentTab = location.pathname === '/analytics' ? 1 : 0
+
+    const handleTabChange = (event, newValue) => {
+        if (newValue === 0) {
+            navigate('/')
+        } else {
+            navigate('/analytics')
+        }
+    }
+}
