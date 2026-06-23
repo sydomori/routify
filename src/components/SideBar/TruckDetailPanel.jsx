68b08bb88bf9ea36ded0fa56fde5a11d7cd51709
sydomori <sydneyomori@gmail.com>
2026-06-23 16:48:31 +0300

    style: add box shadow to truck details panel

--- src/components/SideBar/TruckDetailPanel.jsx
+++ src/components/SideBar/TruckDetailPanel.jsx
@@ -13,7 +13,7 @@ function TruckDetailPanel({selectedTruck}){
                 Selected Truck Details
             </Typography>
 
-            <Box sx={{p:2, backgroundColor: '#fafafa', borderRadius:2, border: '1px solid #e0e0e0'}}>
+            <Box sx={{boxShadow: '0 1px 5px rgba(0, 0, 0, 0.1)', p:2, backgroundColor: '#fafafa', borderRadius:2, border: '1px solid #e0e0e0'}}>
                 {/* truck id + driver */}
                 <Typography variant="body1" fontweight={700}>{selectedTruck.id}</Typography>
                 <Typography variant="caption" sx={{color: '#888'}}>{selectedTruck.driver}</Typography>
