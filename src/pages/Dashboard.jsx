59fefe1f51bd336d5add0f3a8ddf8006379c680a
sydomori <sydneyomori@gmail.com>
2026-06-23 14:36:21 +0300

    feat: wire sidebar to dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,11 @@
 import Toolbar from '@mui/material/Toolbar'
+import SideBar from '../components/SideBar/SideBar'
 function Dashbord({trucks, selectedTruckId, setSelectedTruckId, modalOpen, setModalOpen}){
     return(
         <>
             <Toolbar />
             <div style={{display: 'flex', height: 'calc(100vh - 64px)'}}>
-                {/* Sidebar content goes here */}
+                <SideBar trucks={trucks} selectedTruckId={selectedTruckId} setSelectedTruckId={setSelectedTruckId} />
                 {/* MapPanel goes here */}
                 {/* AddTruckModal goes here */}
             </div>
