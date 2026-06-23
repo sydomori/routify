af93ab8b7e6dd6a4c8e9c2c1d03a1f8802b1807b
sydomori <sydneyomori@gmail.com>
2026-06-23 21:55:54 +0300

    style: add margin to dashboard and increase sidebar width

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -6,7 +6,7 @@ import TruckDetailPanel from './TruckDetailPanel';
 function Sidebar ({trucks, selectedTruckId, setSelectedTruckId}){
     const selectedTruck = trucks.find(truck => truck.id === selectedTruckId);
     return(
-        <Box sx={{width:280,pt:1,pb:2, minWidth: 280, height:'100%', display:'flex', flexDirection:'column', gap:1, backgroundColor: '#fff', overflowY: 'auto'}}>
+        <Box sx={{width:300,pl:1,pt:1,pb:2, minWidth: 280, height:'100%', display:'flex', flexDirection:'column', gap:1, backgroundColor: '#fff', overflowY: 'auto'}}>
             <FleetList trucks={trucks} selectedTruckId={selectedTruckId} setSelectedTruckId={setSelectedTruckId} />
             <TruckDetailPanel selectedTruck={selectedTruck} />
         </Box>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,7 +3,7 @@ import SideBar from '../components/SideBar/SideBar'
 import Box from '@mui/material/Box'
 function Dashboard({trucks, selectedTruckId, setSelectedTruckId, modalOpen, setModalOpen}){
     return(
-        <Box sx={{display: 'flex', flexDirection: 'column', overflow: 'hidden', height:'100vh'}}>
+        <Box sx={{display: 'flex', flexDirection: 'column', overflow: 'hidden', height:'100vh',mt:0.4,mb:1}}>
             <Toolbar />
             <Box style={{display: 'flex',flex:1, overflow: 'hidden'}}>
                 <SideBar trucks={trucks} selectedTruckId={selectedTruckId} setSelectedTruckId={setSelectedTruckId} />
