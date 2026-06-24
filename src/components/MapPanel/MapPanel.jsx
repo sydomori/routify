0750df33adc1cd56cb95824de6959b0d5d1d0854
sydomori <sydneyomori@gmail.com>
2026-06-24 15:40:32 +0300

    style: add styles to mappanel

--- src/components/MapPanel/MapPanel.jsx
+++ src/components/MapPanel/MapPanel.jsx
@@ -4,7 +4,7 @@ import MapView from './MapView';
 {/* a wrapper component that holds the map and passes data to MapView */}
 function MapPanel({trucks, selectedTruckId}){
     return(
-        <Box>
+        <Box sx={{flex:1, position:'relative', height:'100%'}}>
             <MapView trucks={trucks} selectedTruckId={selectedTruckId} />
         </Box>
     )
