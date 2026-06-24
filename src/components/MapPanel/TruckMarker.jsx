3ac802a80cf6899a0de6d6acdf43abb5614272ac
sydomori <sydneyomori@gmail.com>
2026-06-24 16:33:23 +0300

    feat: create selected con marker

---
+++ src/components/MapPanel/TruckMarker.jsx
@@ -0,0 +1,11 @@
+import{Marker, Popup} from 'react-leaflet'
+import L from 'leaflet'
+
+const selectedIcon = new L.Icon({ /* Leaflet's class for creating custom marker icons */
+    iconUrl: '...', /* path to the icon image */
+    shadowUrl: '...', /* path to the shadow image */
+    iconSize: [30,46], /* width and height of the icon image in pixels */
+    iconAnchor: [15,46], /* anchor point of the icon in pixels, relative to the top left corner of the icon image */
+    popupAnchor: [0,-46] /* anchor point of the popup in pixels, relative to the top left corner of the icon image */
+})
+
