80954dc64cf1a4fe1050d692c95425fbdab4f392
sydomori <sydneyomori@gmail.com>
2026-06-24 15:39:38 +0300

    add comment to MapPanel

--- src/components/MapPanel/MapPanel.jsx
+++ src/components/MapPanel/MapPanel.jsx
@@ -1,6 +1,7 @@
 import Box from '@mui/material/Box';
 import MapView from './MapView';
 
+{/* a wrapper component that holds the map and passes data to MapView */}
 function MapPanel({trucks, selectedTruckId}){
     return(
         <Box>
