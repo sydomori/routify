6a82979a24669fe2291805c28ee7d3f2789fb41e
sydomori <sydneyomori@gmail.com>
2026-06-24 17:44:53 +0300

    fx: chaned the truck markers icons

--- src/components/MapPanel/TruckMarker.jsx
+++ src/components/MapPanel/TruckMarker.jsx
@@ -2,16 +2,16 @@ import{Marker, Popup} from 'react-leaflet'
 import L from 'leaflet'
 
 const selectedIcon = new L.Icon({ /* Leaflet's class for creating custom marker icons */
-    iconUrl: '...', /* path to the icon image */
-    shadowUrl: '...', /* path to the shadow image */
+    iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png', /* path to the icon image */
+    shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png', /* path to the shadow image */
     iconSize: [30,46], /* width and height of the icon image in pixels */
     iconAnchor: [15,46], /* anchor point of the icon in pixels, relative to the top left corner of the icon image */
     popupAnchor: [0,-46] /* anchor point of the popup in pixels, relative to the top left corner of the icon image */
 })
 
 const defaultIcon = new L.Icon({ /* Leaflet's class for creating custom marker icons */
-    iconUrl: '...', /* path to the icon image */
-    shadowUrl: '...', /* path to the shadow image */
+    iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png', /* path to the icon image */
+    shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png', /* path to the shadow image */
     iconSize: [20,32], /* smaller that the selected icon to be distinct */
     iconAnchor: [10,32], /* anchor point of the icon in pixels, relative to the top left corner of the icon image */
     popupAnchor: [0,-32] /* anchor point of the popup in pixels, relative to the top left corner of the icon image */
